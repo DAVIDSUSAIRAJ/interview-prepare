9f4a7b765b8a892e636c9655ed20fb4ccb1cd818
David <davidsusairaj1996@gmail.com>
2022-02-10 11:41:54 +0530

    loop 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // OUTPUT
+  // 1
+  // 2 3 2
+  // 3 4 5 4 3
+  // 4 5 6 7 6 5 4
+  // 5 6 7 8 9 8 7 6 5
+
+  for (let i = 1; i <= 5; i++) {
+    if (i == 1) {
+      console.log(i);
+    } else {
+      console.log("/n");
+      let value = 0;
+      for (let j = 1; j <= i; j++) {
+        if (j == 1) {
+          value = i;
+          console.log(value);
+        } else {
+          value = value + 1;
+          console.log(value);
+        }
+      }
+      for (let j = 1; j < i; j++) {
+        value = value - 1;
+        console.log(value);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello interview</h1>
     </div>
   );
 }
