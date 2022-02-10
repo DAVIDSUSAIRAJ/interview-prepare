477b43f3f19f3d7130255d0fbd20533b5ee0391e
David <davidsusairaj1996@gmail.com>
2022-02-11 03:51:59 +0530

    day 2 prepare for interview

--- src/App.js
+++ src/App.js
@@ -51,38 +51,81 @@ function App() {
 
   // THIRD METHOD IN LOOP
 
-  for (let i = 1; i <= 5; i++) {
-    if (i == 1) {
-      let arr = [];
-      arr.push("1");
-      let one = arr.join();
-      console.log(one);
-    }
-    if (i == 2) {
-      let arr2 = [];
-      arr2.push("2 3 2");
-      let two = arr2.join();
-      console.log(two);
-    }
-    // THIS IS LITTL DIFFUCULT
-    if (i == 3) {
-      let arr3 = [];
-      for (let i = 1; i <= 5; i++) {
-        if (i == 1) {
-          arr3.push(3);
-        }
-        if (i == 2) {
-          arr3.push(4);
-        }
-        if (i == 3) {
-          arr3.push(5);
-        }
-      }
-      console.log(arr3.length);
-      let joi = arr3.join(" ");
-      console.log(joi);
-    }
-  }
+  // for (let i = 1; i <= 5; i++) {
+  //   if (i == 1) {
+  //     let arr = [];
+  //     arr.push("1");
+  //     let one = arr.join();
+  //     console.log(one);
+  //   }
+  //   if (i == 2) {
+  //     let arr2 = [];
+  //     arr2.push("2 3 2");
+  //     let two = arr2.join();
+  //     console.log(two);
+  //   }
+  // THIS IS LITTLe defucult loop
+  //   if (i == 3) {
+  //     let arr3 = [];
+  //     for (let i = 1; i <= 5; i++) {
+  //       if (i == 1) {
+  //         arr3.push(3);
+  //       }
+  //       if (i == 2) {
+  //         arr3.push(4);
+  //       }
+  //       if (i == 3) {
+  //         arr3.push(5);
+  //       }
+  //     }
+  //     console.log(arr3.length);
+  //     let joi = arr3.join(" ");
+  //     console.log(joi);
+  //   }
+  // }
+  // let name = "david";
+  // console.log(name);
+  // let ar = [2, 4, 6];
+  // console.log(ar);
+
+  // LETTER REVERSING
+
+  // let text = "How are you doing today?";
+  // let myArray = text.split(" ");
+  // console.log(myArray);
+  // let revr = myArray.reverse();
+
+  // let rverse = revr.join("");
+  // let rverseVal = rverse.split("");
+  // console.log(rverseVal);
+  // let newArr = [];
+  // rverseVal.forEach((val, ind) => {
+  //   if (!newArr.includes(val)) {
+  //     newArr.push(val);
+  //   }
+  // });
+  // console.log(newArr);
+  // let main = newArr.reverse();
+  // let mainResult = main.join("");
+  // console.log(mainResult);
+  // END REVERSING ARRAY
+
+  // {...obj} deep copy only primitive types
+  // Object.assign(objName) SHALLOW COPY
+  // Object.assign({},objName); deep copy
+  // JSON.parse(JSON.stringify(objName)) deep copy
+  // let x = new String("davdi");
+  // Boolean(true);
+  // new Boolean(true)
+  // let y = new Boolean(false);
+  // console.log(y);
+  // console.log(x);
+  // let no = 5;
+  // console.log(no);
+
+  // let foo = ["le1", "le2", "le3", "le4"];
+  // console.log(foo.length);
+  // console.log(foo.length);
 
   return (
     <div className="App">
