1cb88a63a3459bded945b48f71ce2b3d432a3c6d
David <davidsusairaj1996@gmail.com>
2022-02-10 12:40:46 +0530

    loop 3

--- src/App.js
+++ src/App.js
@@ -30,22 +30,57 @@ function App() {
   //   }
   // }
   // SECON METHOD THIS IS FUNNY
+  // for (let i = 1; i <= 5; i++) {
+  //   switch (i) {
+  //     case 1:
+  //       console.log("1");
+  //       break;
+  //     case 2:
+  //       console.log("2 3 2");
+  //       break;
+  //     case 3:
+  //       console.log("3 4 5 4 3");
+  //       break;
+  //     case 4:
+  //       console.log("4 5 6 7 6 5 4");
+  //       break;
+  //     case 5:
+  //       console.log("5 6 7 8 9 8 7 6 5");
+  //   }
+  // }
+
+  // THIRD METHOD IN LOOP
+
   for (let i = 1; i <= 5; i++) {
-    switch (i) {
-      case 1:
-        console.log("1");
-        break;
-      case 2:
-        console.log("2 3 2");
-        break;
-      case 3:
-        console.log("3 4 5 4 3");
-        break;
-      case 4:
-        console.log("4 5 6 7 6 5 4");
-        break;
-      case 5:
-        console.log("5 6 7 8 9 8 7 6 5");
+    if (i == 1) {
+      let arr = [];
+      arr.push("1");
+      let one = arr.join();
+      console.log(one);
+    }
+    if (i == 2) {
+      let arr2 = [];
+      arr2.push("2 3 2");
+      let two = arr2.join();
+      console.log(two);
+    }
+    // THIS IS LITTL DIFFUCULT
+    if (i == 3) {
+      let arr3 = [];
+      for (let i = 1; i <= 5; i++) {
+        if (i == 1) {
+          arr3.push(3);
+        }
+        if (i == 2) {
+          arr3.push(4);
+        }
+        if (i == 3) {
+          arr3.push(5);
+        }
+      }
+      console.log(arr3.length);
+      let joi = arr3.join(" ");
+      console.log(joi);
     }
   }
 
